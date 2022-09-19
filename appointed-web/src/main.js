df9c8391b89fc45d8d8ed4085116cb5e7c62b5df
huanghe <h13299630387@163.com>
2022-09-19 18:51:36 +0800

    删除无用文件

--- appointed-web/src/main.js
+++ appointed-web/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import './utils/common';
+
 // 第三方组件库注册
 import componentInit from '@/components/otherComponents';
 
